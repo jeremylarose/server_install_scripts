7da5c7a41a13c92583bc18e52175aa302d5ebb56
Jeremy Larose <jeremy.larose@protonmail.com>
2018-09-06 14:50:33 -0400

    Update and rename Gitea1.5.0_MariaDB10.3_Ubuntu18.04 to Gitea_Ubuntu.sh

---
+++ Gitea_Ubuntu.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# install mysql or mariadb seperately (ex: ./MariaDB_CentOS.sh -r rootpassword -d gitea - u gitea -p dbpassword)
+
+# Version numbers
+GOVERSION="1.10.3"
+GITEA_VERSION="1.5.0"
+
+# Install prereqs
+apt-get -y install make gcc libpam-dev build-essential git
+
+# If apt fails to run completely the rest of this isn't going to work...
+if [ $? -ne 0 ]; then
+    echo "apt-get failed to install all required dependencies"
+    exit
+fi
+
+# Download GO to build gitea
+wget https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz
+if [ $? -ne 0 ]; then
+    echo "Failed to download go${GOVERSION}.linux-amd64.tar.gz"
+    echo "https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz"
+    exit
+fi
+
+# Extract GO
+tar -xzf go${GOVERSION}.linux-amd64.tar.gz
+
+# set temporary environment variables
+CWD=$(pwd)
+export GOROOT=$CWD/go
+export GOPATH=$CWD/go/working
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# create gitea binary with pam using go
+go get -d -u code.gitea.io/gitea
+cd $GOPATH/src/code.gitea.io/gitea
+git checkout v${GITEA_VERSION}
+
+TAGS="pam bindata" make generate build
+
+cp -f $GOPATH/src/code.gitea.io/gitea/gitea /usr/local/bin
+
+# create git user to run gitea
+adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Git Version Control' \
+   --group \
+   --disabled-password \
+   --home /home/git \
+   git
+
+# allow git user to authenticate pam by adding to shadow group
+usermod -a -G shadow git
+
+# create required directory structure
+mkdir -p /var/lib/gitea/{custom,data,indexers,public,log}
+chown git:git /var/lib/gitea/{data,indexers,log}
+chmod 750 /var/lib/gitea/{data,indexers,log}
+mkdir /etc/gitea
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+
+# copy gitea systemd service
+cp $GOPATH/src/code.gitea.io/gitea/contrib/systemd/gitea.service /etc/systemd/system/gitea.service
+
+# uncomment mysqld.service in gitea systemd service
+sed -i '/mysqld.service/s/^#//g' /etc/systemd/system/gitea.service
+
+# enable and start gitea service
+systemctl enable gitea
+systemctl start gitea
+
+echo -e "Installation complete, point your browser to http://server:3000
+|        to configure your new Gitea installation."
