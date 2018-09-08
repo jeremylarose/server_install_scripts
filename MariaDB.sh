49364d0f9144d50d572c9c14313b95820659b988
Jeremy Larose <jeremy.larose@protonmail.com>
2018-09-07 21:01:47 -0400

    Update MariaDB.sh

--- MariaDB.sh
+++ MariaDB.sh
@@ -97,16 +97,8 @@ if [ -z "$dbpwd" ]; then
     echo
 fi
 
-# install only if mysql not already installed AND os family matches
-mysql --version
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo
-  echo mysql already installed
-  echo
-  mysql --version
-  echo
-elif [ $RESULT -ne 0 ] && [ $os_family = debian ]; then
+# install MariaDB
+if [ $os_family = debian ]; then
   # install MariaDB bypassing password prompt
   debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password password $rootpwd"
   debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password_again password $rootpwd"
@@ -121,8 +113,7 @@ elif [ $RESULT -ne 0 ] && [ $os_family = debian ]; then
   if [ $? -eq 0 ]; then
     echo "MariaDB $mariadb_version installed successfully"
   fi
-
-elif [ $RESULT -ne 0 ] && [ $os_family = fedora ]; then
+elif [ $os_family = fedora ]; then
 # add MariaDB repo for centos
 cat <<EOF >/etc/yum.repos.d/mariadb.repo
 [mariadb]
