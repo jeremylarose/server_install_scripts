30c08d04d0da03073d48cf1d5c3b65464b421aa1
Jeremy Larose <jeremy.larose@protonmail.com>
2018-11-12 10:13:36 -0500

    Update SaltGUI.sh

--- SaltGUI.sh
+++ SaltGUI.sh
@@ -6,7 +6,7 @@
 # OR
 # ./filename.sh -u username -p password -v saltguiversionnumber
 
-saltgui_version="1.4.0"
+saltgui_version="1.4.1"
 
 # Get script arguments for non-interactive mode
 while [ "$1" != "" ]; do
