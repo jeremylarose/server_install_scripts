32d627b03d567790df6b3fcee1bb6a2d9de6c1d2
Jeremy <jeremy.larose@gmail.com>
2019-03-07 21:32:56 -0500

    Test

--- MunkiReport_TESTING.sh
+++ MunkiReport_TESTING.sh
@@ -142,8 +142,8 @@ if [ $DATABASE = sqlite ]; then
 	EOF
 elif [ $DATABASE = mysql ]; then
   cat <<-EOF >${MUNKIREPORT_LOCATION}/.env
-  CONNECTION_DRIVER="mysql"
-  CONNECTION_HOST="${MYSQL_HOST}"
+	CONNECTION_DRIVER="mysql"
+	CONNECTION_HOST="${MYSQL_HOST}"
   CONNECTION_PORT="${MYSQL_HOSTPORT}"
   CONNECTION_DATABASE="munkireport"
   CONNECTION_USERNAME="${MYSQL_DBUSER}"
@@ -152,7 +152,7 @@ elif [ $DATABASE = mysql ]; then
   CONNECTION_COLLATION="utf8mb4_unicode_ci"
   CONNECTION_STRICT=TRUE
   CONNECTION_ENGINE="InnoDB"
-  EOF
+	EOF
 fi
 
 # Copy across the old configuration files overwiting new
