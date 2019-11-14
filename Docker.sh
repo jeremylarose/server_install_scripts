902bbf9dffe5ece186363ad3177934f9014ba5a7
Jeremy Larose <jeremy.larose@protonmail.com>
2019-11-14 13:09:53 -0500

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -7,7 +7,7 @@
 # ./filename.sh -v dockerversion
 
 # default variables unless specified from command line
-DOCKER_VERSION="5:19.03.3~3-0"
+DOCKER_VERSION="5:19.03.4~3-0"
 
 # get os from system
 os=`cat /etc/*release | grep ^ID= | cut -d= -f2 | sed 's/\"//g'`
