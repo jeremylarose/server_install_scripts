a68185cceeb0afe0709517552723d9bd555d7e8d
Jeremy Larose <jeremy.larose@protonmail.com>
2019-11-21 12:48:04 -0500

    Update SimpleHelp.sh

--- SimpleHelp.sh
+++ SimpleHelp.sh
@@ -7,7 +7,7 @@
 # ./filename.sh -v version -s service
 
 # default variables unless specified from command line
-SIMPLEHELP_VERSION="5.1.5"
+SIMPLEHELP_VERSION="5.1.8"
 
 # set default service as suggested by SimpleHelp
 if [ -f /sbin/initctl ]; then
