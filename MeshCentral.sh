47ec2b1ccce40f6c080515b41301f1f0d0e9479b
Jeremy Larose <jeremy.larose@protonmail.com>
2020-03-04 09:12:28 -0500

    Update MeshCentral.sh

--- MeshCentral.sh
+++ MeshCentral.sh
@@ -8,15 +8,29 @@
 
 # default variables unless specified from command line
 MC_VERSION="0.4.9-u"
+MONGODB_VERSION="4.2"
 
 # get os from system
 os=`cat /etc/*release | grep ^ID= | cut -d= -f2 | sed 's/\"//g'`
+
+# get os version id from system
+osversion_id=`cat /etc/*release | grep ^VERSION_ID= | cut -d= -f2 | sed 's/\"//g' | cut -d. -f1`
 
 # get os family from system
 if [ $os = debian ] || [ $os = fedora ]; then
   os_family=$os
 else
   os_family=`cat /etc/*release | grep ^ID_LIKE= | cut -d= -f2 | sed 's/\"//g' | cut -d' ' -f2`
+fi
+
+# get os_codename from system
+if [ $os = debian ] || [ $os = centos ] || [ $os = rhel ]; then
+  os_codename=`cat /etc/*release | grep ^VERSION= | cut -d'(' -f2 | cut -d')' -f1 | awk '{print tolower($0)}'`
+elif [ $os = ubuntu ]; then
+  os_codename=`cat /etc/*release | grep ^DISTRIB_CODENAME= | cut -d= -f2`
+else
+  echo "unknown os codename"
+  exit 1
 fi
 
 # Get script arguments for non-interactive mode
@@ -32,11 +46,27 @@ done
 
 # install prereqs
 if [ $os_family = debian ]; then
-  apt-get -y install wget nodejs npm
+  # install nodejs 10 and npm
+  apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+  apt update
+  apt -y install gcc g++ make
+  apt -y install nodejs npm
+  
   # install mongodb
-  apt-get -y install mongodb
-  systemctl start mongodb
-  systemctl enable mongodb
+  apt -y install gnupg
+  wget -qO - https://www.mongodb.org/static/pgp/server-$MONGODB_VERSION.asc | sudo apt-key add -
+  if [ $os = debian ]; then
+  echo "deb http://repo.mongodb.org/apt/$os $os_codename/mongodb-org/$MONGODB_VERSION main" | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGODB_VERSION.list
+  elif [ $os = ubuntu ]; then
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/$os $os_codename/mongodb-org/$MONGODB_VERSION multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGODB_VERSION.list
+  fi
+  apt update
+  apt -y install mongodb-org
+  systemctl daemon-reload
+  systemctl enable mongod
+  systemctl start mongod
+  
 elif [ $os_family = fedora ]; then
   echo "os not supported yet"
   exit
@@ -95,3 +125,5 @@ systemctl enable meshcentral.service
 systemctl enable meshcentral.service
 
 service meshcentral start
+
+echo -e "Installation complete, wait a bit for service to start the first time and to got https://localhost to access site"
