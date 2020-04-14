a2cedaab53116c2f312df93295359179f355b156
Jeremy Larose <jeremy.larose@protonmail.com>
2020-04-13 21:39:18 -0400

    Update OCSInventoryNG.sh

--- OCSInventoryNG.sh
+++ OCSInventoryNG.sh
@@ -9,7 +9,7 @@
 # install mysql or mariadb seperately (ex: ./MariaDB.sh -r rootpassword -d ocsweb -u ocs_dbuser -p dbpassword)
 
 # OCS Inventory defaults unless specified with command line argument
-ocsversion="2.6"
+ocsversion="2.7"
 ocsdbhost="localhost"
 ocsdbhostport="3306"
 
