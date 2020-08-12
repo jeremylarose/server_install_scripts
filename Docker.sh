5439b09c28ecad8d14f3ee5e2414440f7b7c31ca
Jeremy Larose <jeremy.larose@protonmail.com>
2020-08-12 13:03:47 -0400

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -46,7 +46,7 @@ done
 # Install Docker
 if [ $os_family = debian ]; then
   apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-  curl -fsSL https://download.docker.com/linux/${os}/gpg | sudo apt-key add -
+  curl -fsSL https://download.docker.com/linux/${os}/gpg | apt-key add -
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${os} ${os_codename} stable"
   apt update
   apt -y install docker-ce=${DOCKER_VERSION}~${os}-${os_codename}
