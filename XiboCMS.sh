c57bf0f339b0de6119a19de06ad970818100ca40
larosejm <larosejm@vt.edu>
2020-08-31 12:29:06 -0400

    Remove external mysql

--- XiboCMS.sh
+++ XiboCMS.sh
@@ -2,41 +2,22 @@
 
 # first make executable with chmod +x filename.sh
 # then run with ./filename.sh
-# or automated with ./filename.sh --version xibo_version --composeversion docker-compose_version --dbname databasename --dbuser username --dbpwd password --dbhost hostname --dbhostportnumber portnumber --webport cmswebport
+# or automated with ./filename.sh --version xibo_version --composeversion docker-compose_version --dbpwd password --xmrport xmrport --webport cmswebport
 # OR
-# ./filename.sh -v version -cv compose-version -d databasename -u username -p password -h hostname -n hostportnumber - wp webport
-
-# install mysql or mariadb seperately (ex: ./MariaDB.sh -r rootpassword -d cms -u cms -p dbpassword)
+# ./filename.sh -v version -cv compose-version -p password -xp xmrport -wp webport
 
 # default variables unless specified from command line
 XIBO_VERSION="2.3.6"
 DOCKERCOMPOSE_VERSION="1.26.2"
-xibodbhost="localhost"
-xibodbhostport="3306"
-xibowebport="80"
+xiboxmrport=65500
+xibowebport=9505
 
 # Get script arguments for non-interactive mode
 while [ "$1" != "" ]; do
     case $1 in
-        -d | --dbname )
-            shift
-            xibodbname="$1"
-            ;;
-        -u | --dbuser )
-            shift
-            xibodbuser="$1"
-            ;;
         -p | --dbpwd )
             shift
             xibodbpwd="$1"
-            ;;
-        -h | --dbhost )
-            shift
-            xibodbhost="$1"
-            ;;
-        -n | --dbhostportnumber )
-            shift
-            xibodbhostport="$1"
             ;;
         -v | --version )
             shift
@@ -46,24 +27,10 @@ while [ "$1" != "" ]; do
             shift
             DOCKERCOMPOSE_VERSION="$1"
             ;;
-        -wp | --webport )
-            shift
-            xibowebport="$1"
-            ;;
     esac
     shift
 done
 
-if [ -z "$xibodbname" ]; then
-    echo
-    read -p "Enter the Xibo Database name: " xibodbname
-    echo
-fi
-if [ -z "$xibodbuser" ]; then
-    echo
-    read -p "Enter a username with permissions to $xibodbname: " xibodbuser
-    echo
-fi
 if [ -z "$xibodbpwd" ]; then
     echo
     while true
@@ -85,7 +52,6 @@ chmod +x /usr/local/bin/docker-compose
 if [ $? -ne 0 ]; then
     echo "Failed to download docker-compose ${DOCKERCOMPOSE_VERSION}"
 fi
-
 
 # backup current version if installed
 now=$(date +"%Y%m%d_%H%M%S")
@@ -117,21 +83,27 @@ fi
 
 # copy config file if not exist and set mysql password
 if [[ ! -f /opt/xibo/config.env ]]; then
-cat <<EOF >/opt/xibo/config.env
-MYSQL_HOST=$xibodbhost
-MYSQL_PORT=$xibodbhostport
-MYSQL_DATABASE=$xibodbname
-MYSQL_USER=$xibodbuser
-MYSQL_PASSWORD=$xibodbpwd
-EOF
+cp /opt/xibo/config.env.template /opt/xibo/config.env
+XIBO_MYSQLPW_REPLACETEXT='MYSQL_PASSWORD='
+XIBO_MYSQLPW_RESTAPI_NEW='MYSQL_PASSWORD=zreplaceholder'
+sed -i "/$XIBO_MYSQLPW_REPLACETEXT/c $XIBO_MYSQLPW_NEW" /opt/xibo/config.env
+sed -i "s/zreplaceholder/$xibodbpwd/" /opt/xibo/config.env
 fi
 
-# Bring CMS up with Docker Compose and specified port
-XIBO_WEBPORT_REPLACETEXT='80:80'
+# Bring CMS up with Docker Compose and specified ports
+if [[ ! -f /opt/xibo/cms_custom-ports.yml ]]; then
+cp /opt/xibo/cms_custom-ports.yml.template /opt/xibo/cms_custom-ports.yml
+XIBO_WEBPORT_REPLACETEXT='65500:9505'
+XIBO_WEBPORT_NEW="$xiboxmrport:80"
+sed -i "s/$XIBO_WEBPORT_REPLACETEXT/$XIBO_WEBPORT_NEW/g" /opt/xibo/cms_custom-ports.yml
+XIBO_WEBPORT_REPLACETEXT='65501:80'
 XIBO_WEBPORT_NEW="$xibowebport:80"
-sed -i "s/$XIBO_WEBPORT_REPLACETEXT/$XIBO_WEBPORT_NEW/g" /opt/xibo/cms_remote-mysql.yml
+sed -i "s/$XIBO_WEBPORT_REPLACETEXT/$XIBO_WEBPORT_NEW/g" /opt/xibo/cms_custom-ports.yml
+fi
+
 cd /opt/xibo
-docker-compose -f cms_remote-mysql.yml up -d
+docker-compose -f cms_custom-ports.yml up -d
 
 echo "Xibo CMS $XIBO_VERSION setup/upgrade complete https://xibo.org.uk/docs/setup/cms-installation-guides"
+echo "http://yourserverhere:$xibowebport"
 echo "default username is xibo_admin, default password is password"
