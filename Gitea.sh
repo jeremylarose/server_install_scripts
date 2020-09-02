48f468c702ff12934b3fe8d639b6e7601336513d
Jeremy Larose <jeremy.larose@protonmail.com>
2020-09-02 15:20:11 -0400

    Update Gitea.sh

--- Gitea.sh
+++ Gitea.sh
@@ -2,8 +2,7 @@
 
 # install mysql or mariadb seperately (ex: ./MariaDB.sh -r rootpassword -d gitea - u gitea -p dbpassword)
 
-# Version numbers
-GOVERSION="1.15.1"
+# Version number
 GITEA_VERSION="1.12.3"
 
 # get os from system
@@ -22,22 +21,13 @@ if [ $os_family = debian ]; then
   # allow git user to authenticate pam by adding to shadow group
   usermod -a -G shadow git
   # install prereqs
-  apt-get -y install make gcc libpam-dev build-essential git
-  # install nodejs 10
-  apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-  curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
-  apt update
-  apt -y install gcc g++ make
-  apt -y install nodejs
+  apt-get -y install wget make gcc libpam-dev build-essential git
 elif [ $os_family = fedora ]; then  
   # create git user to run gitea
   adduser git -s /bin/bash -d /home/git
   mkhomedir_helper git
   # install prereqs
   yum -y install make gcc pam-devel wget git
-  # install nodejs 10
-  curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-  yum -y install nodejs
 else
   echo "unknown operating system family"
   exit 1
@@ -49,33 +39,10 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-# Download GO to build gitea
-wget https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz
-if [ $? -ne 0 ]; then
-    echo "Failed to download go${GOVERSION}.linux-amd64.tar.gz"
-    echo "https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz"
-    exit
-fi
-
-# Extract GO
-tar -xzf go${GOVERSION}.linux-amd64.tar.gz
-
-# set temporary environment variables
-CWD=$(pwd)
-export GOROOT=$CWD/go
-export GOPATH=$CWD/go/working
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-
-# create gitea binary with pam using go
-go get -d -u code.gitea.io/gitea
-cd $GOPATH/src/code.gitea.io/gitea
-git reset -- hard
-git pull
-git checkout v${GITEA_VERSION}
-
-TAGS="bindata" make build
-
-cp -f $GOPATH/src/code.gitea.io/gitea/gitea /usr/local/bin
+# stop service if running and download new verison of gitea
+systemctl is-active --quiet gitea && systemctl stop gitea
+wget -O /usr/local/bin/gitea https://dl.gitea.io/gitea/${GITEA_VERSION}/gitea-${GITEA_VERSION}-linux-amd64
+chmod +x /usr/local/bin/gitea
 
 # create required directory structure
 mkdir -p /var/lib/gitea/{custom,data,indexers,public,log}
@@ -85,19 +52,38 @@ mkdir -p /etc/gitea
 chown root:git /etc/gitea
 chmod 770 /etc/gitea
 
-# copy gitea systemd service
-cp $GOPATH/src/code.gitea.io/gitea/contrib/systemd/gitea.service /etc/systemd/system/gitea.service
+# create systemd service and start gitea
+if [[ -e /etc/systemd/system/ ]]; then
+cat <<-EOF >/etc/systemd/system/gitea.service
+[Unit]
+Description=Gitea (Git with a cup of tea)
+After=syslog.target
+After=network.target
+Requires=mysql.service
 
-# remove go files
-rm -rf $GOROOT
+[Service]
+LimitMEMLOCK=infinity
+LimitNOFILE=65535
+RestartSec=2s
+Type=simple
+User=git
+Group=git
+WorkingDirectory=/var/lib/gitea/
+ExecStart=/usr/local/bin/gitea web --config /etc/gitea/app.ini
+Restart=always
+Environment=USER=git HOME=/home/git GITEA_WORK_DIR=/var/lib/gitea
+CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_BIND_SERVICE
 
-# uncomment mysqld.service in gitea systemd service
-sed -i '/mysqld.service/s/^#//g' /etc/systemd/system/gitea.service
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
 
 # enable and start gitea service
+systemctl daemon-reload
 systemctl enable gitea
 systemctl start gitea
-systemctl restart gitea
 
 echo -e "Installation complete, point your browser to http://server:3000
 |        to configure your new Gitea installation."
