ffadc2f20bcacdcaa4b59a1584ea458450275f03
Jeremy Larose <jeremy.larose@protonmail.com>
2020-10-20 15:34:17 -0400

    Update OCSInventoryNG.sh

--- OCSInventoryNG.sh
+++ OCSInventoryNG.sh
@@ -9,7 +9,7 @@
 # install mysql or mariadb seperately (ex: ./MariaDB.sh -r rootpassword -d ocsweb -u ocs_dbuser -p dbpassword)
 
 # OCS Inventory defaults unless specified with command line argument
-ocsversion="2.7"
+ocsversion="2.8"
 ocsdbhost="localhost"
 ocsdbhostport="3306"
 
@@ -85,17 +85,17 @@ fi
 
 # Install prereqs
 if [ $os_family = debian ]; then
-  apt -y install wget gpg software-properties-common
+  apt -y install wget gpg software-properties-common ca-certificates apt-transport-https 
   add-apt-repository -y ppa:ondrej/php
   apt update
-  apt-get -y install php7.3-curl apache2-dev gcc perl-modules make apache2 perl libapache2-mod-perl2 libapache2-mod-php \
-  libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php7.3-mysql \
-  php7.3 php7.3-gd php7.3-dev php7.3-mbstring php7.3-soap php7.3-xml php-pclzip libarchive-zip-perl php7.3-zip cpanminus
+  apt-get -y install php7.4-curl apache2-dev gcc perl-modules make apache2 perl libapache2-mod-perl2 libapache2-mod-php \
+  libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php7.4-mysql \
+  php7.4 php7.4-gd php7.4-dev php7.4-mbstring php7.4-soap php7.4-xml php-pclzip libarchive-zip-perl php7.4-zip cpanminus
 
 elif [ $os_family = fedora ] && [ $osversion_id = 8 ]; then
   yum -y install epel-release wget tar
   rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-${osversion_id}.rpm
-  dnf module install -y php:remi-7.3
+  dnf module install -y php:remi-7.4
   dnf install -y php-mysqlnd
   yum install -y httpd httpd-devel gcc mod_perl mod_php mod_ssl make perl-XML-Simple perl-Compress-Zlib perl-DBI \
   perl-DBD-MySQL perl-Net-IP perl-Archive-Zip cpanminus php-curl php-common php-gd php-mbstring php-soap php-mysql php-ldap php-xml
@@ -103,10 +103,10 @@ elif [ $os_family = fedora ] && [ $osversion_id = 8 ]; then
 elif [ $os_family = fedora ]; then
   # install prerequisites
   yum -y install epel-release wget
-  # add Remi repo for php 7.3
+  # add Remi repo for php 7.4
   rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-${osversion_id}.rpm
   # Install more prereqs
-  yum install --enablerepo=remi-php73 -y httpd httpd-devel gcc mod_perl mod_php mod_ssl make perl-XML-Simple perl-Compress-Zlib perl-DBI \
+  yum install --enablerepo=remi-php74 -y httpd httpd-devel gcc mod_perl mod_php mod_ssl make perl-XML-Simple perl-Compress-Zlib perl-DBI \
   perl-DBD-MySQL perl-Net-IP perl-SOAP-Lite perl-Archive-Zip cpanminus php-curl php-common php-gd php-mbstring php-soap php-mysql php-ldap php-xml
   # enable and start httpd
   systemctl enable httpd
