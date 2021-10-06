f673a027f27a55b25a7fd6ae09d03088587b2b36
Jeremy Larose <jeremy.larose@protonmail.com>
2021-10-06 15:01:42 -0400

    Update OCSInventoryNG.sh

--- OCSInventoryNG.sh
+++ OCSInventoryNG.sh
@@ -88,9 +88,9 @@ if [ $os_family = debian ]; then
   apt -y install wget gpg software-properties-common ca-certificates apt-transport-https 
   add-apt-repository -y ppa:ondrej/php
   apt update
-  apt-get -y install php7.4-curl apache2-dev gcc perl-modules make apache2 perl libapache2-mod-perl2 libapache2-mod-php \
-  libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php7.4-mysql \
-  php7.4 php7.4-gd php7.4-dev php7.4-mbstring php7.4-soap php7.4-xml php-pclzip libarchive-zip-perl php7.4-zip cpanminus
+  apt-get -y install php8.0-curl apache2-dev gcc perl-modules make apache2 perl libapache2-mod-perl2 libapache2-mod-php \
+  libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php8.0-mysql \
+  php8.0 php8.0-gd php8.0-dev php8.0-mbstring php8.0-soap php8.0-xml php-pclzip libarchive-zip-perl php8.0-zip cpanminus
 
 elif [ $os_family = fedora ] && [ $osversion_id = 8 ]; then
   yum -y install epel-release wget tar
