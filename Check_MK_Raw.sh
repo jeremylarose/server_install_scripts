80ac0a56f2a8688e4eac484440a5a3c89e6e4f8e
Jeremy Larose <jeremy.larose@protonmail.com>
2022-05-11 10:23:24 -0400

    Update Check_MK_Raw.sh

--- Check_MK_Raw.sh
+++ Check_MK_Raw.sh
@@ -7,7 +7,7 @@
 # ./filename.sh -v version -s sitename
 
 # default variables unless specified from command line
-CHECK_MK_VERSION="2.0.0p23"
+CHECK_MK_VERSION="2.0.0p24"
 SITENAME="monitoring"
 
 # get os from system
