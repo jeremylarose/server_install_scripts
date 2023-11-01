b2231b7a418d492fd9ea7a8229c069f3556a37d7
Jeremy Larose <jeremy.larose@protonmail.com>
2023-11-01 16:07:56 -0400

    Update MeshCentral.sh

--- MeshCentral.sh
+++ MeshCentral.sh
@@ -50,7 +50,7 @@ if [ $os_family = debian ]; then
   # install nodejs 16 and npm
   apt -y install ca-certificates curl gnupg
   mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor --batch --yes -o /etc/apt/keyrings/nodesource.gpg
   echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   apt update
   apt -y install nodejs
