6cb8b10e67f2be03a9a5702a839df135ae774a67
Jeremy Larose <jeremy.larose@protonmail.com>
2025-10-30 08:17:03 -0400

    Update Gitea.sh

--- Gitea.sh
+++ Gitea.sh
@@ -9,7 +9,7 @@
 # install mysql or mariadb seperately (ex: ./MariaDB.sh -r rootpassword -d gitea - u gitea -p dbpassword)
 
 # Version number
-GITEA_VERSION="1.24.7"
+GITEA_VERSION="1.25.0"
 
 # get os from system
 os=`cat /etc/*release | grep ^ID= | cut -d= -f2 | sed 's/\"//g'`
